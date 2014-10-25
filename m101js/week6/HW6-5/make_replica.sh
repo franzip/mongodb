239527b4603bbed8557f99562e269cc43ba18f8f
franzip <franzpezzella@gmail.com>
2014-10-25 10:03:29 +0200

    Initial commit.

---
+++ m101js/week6/HW6-5/make_replica.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir -p /data/db/rs1 /data/db/rs2 /data/db/rs3
+
+mongod --replSet m101 --logpath "1.log" --dbpath /data/rs1 --port 27017 --smallfiles --oplogSize 64 --fork
+mongod --replSet m101 --logpath "2.log" --dbpath /data/rs2 --port 27018 --smallfiles --oplogSize 64 --fork
+mongod --replSet m101 --logpath "3.log" --dbpath /data/rs3 --port 27019 --smallfiles --oplogSize 64 --fork
+
+mongo --port 27017 << 'EOF'
+config = { _id: "m101", members:[
+          { _id : 0, host : "localhost:27017"},
+          { _id : 1, host : "localhost:27018"},
+          { _id : 2, host : "localhost:27019"} ]
+};
+rs.initiate(config);
+rs.status();
+EOF
